77a9e8f3d3e5703aa0fcf06f4e0c92d9af528cc5
Nicholas Kincaid <nbkincaid@gmail.com>
2016-12-04 16:27:36 -0600

    add classifiers

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name='py-responsys',
   packages=['py-responsys'],  # this must be the same as the name above
-  version='0.1.3',
+  version='0.1.4',
   description='This is an Oracle Responsys REST API client written in Python 2.7.',
   author='Nicholas Kincaid',
   author_email='nbkincaid@gmail.com',
   url='https://github.com/spothero/py-responsys',
-  download_url='https://github.com/spothero/py-responsys/tarball/0.1.3',
-  keywords=[],
-  classifiers=[],
+  download_url='https://github.com/spothero/py-responsys/tarball/0.1.4',
+  keywords=['testing'],
+  classifiers=["Programming Language :: Python :: 2.7",
+               "Topic :: Software Development :: Libraries :: Python Modules"]
 )

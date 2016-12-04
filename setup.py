45c40081fd91d0ecd90aed3c98572974686ee5f0
Nicholas Kincaid <nbkincaid@gmail.com>
2016-12-04 02:04:29 -0600

    version 0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='py-responsys',
   packages=['py-responsys'],  # this must be the same as the name above
-  version='0.1.2',
+  version='0.1.3',
   description='This is an Oracle Responsys REST API client written in Python 2.7.',
   author='Nicholas Kincaid',
   author_email='nbkincaid@gmail.com',
   url='https://github.com/spothero/py-responsys',
-  download_url='https://github.com/spothero/py-responsys/tarball/0.1',
+  download_url='https://github.com/spothero/py-responsys/tarball/0.1.3',
   keywords=[],
   classifiers=[],
 )

d3ee4d0be59af5f0e162229673c16fa249abd810
Nicholas Kincaid <nbkincaid@gmail.com>
2016-12-04 01:20:09 -0600

    add setup files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='py-responsys',
+  packages=['py-responsys'],  # this must be the same as the name above
+  version='0.1',
+  description='This is an Oracle Responsys REST API client written in Python 2.7.',
+  author='Nicholas Kincaid',
+  author_email='nbkincaid@gmail.com',
+  url='https://github.com/spothero/py-responsys',
+  download_url='https://github.com/spothero/py-responsys/tarball/0.1',
+  keywords=[],
+  classifiers=[],
+)

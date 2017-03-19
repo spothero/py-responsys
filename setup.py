1612ad48b823784aee1be206f3c3416a68106b22
Nicholas Kincaid <nbkincaid@gmail.com>
2017-03-19 17:01:17 -0500

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name='responsys_client',
   packages=['responsys_client'],  # this must be the same as the name above
-  version='0.1.4',
+  version='0.1.5',
   description='This is an Oracle Responsys REST API client written in Python 2.7.',
   author='Nicholas Kincaid',
   author_email='nbkincaid@gmail.com',

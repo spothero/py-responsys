137ab5fd9dd127fd39aabdf8d9787bd3ba25eaf8
Andrei Shabanski <shabanski.andrei@gmail.com>
2017-05-25 19:18:07 +0300

    Added "mock" to the dependency list

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         'pytz==2016.7',
         'requests==2.12.0',
+        'mock==2.0.0',
     ],
     description='This is an Oracle Responsys REST API client written in Python 2.7.',
     author='Nicholas Kincaid',

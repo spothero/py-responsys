3c04f80327c96f6927e9f751455baf51f1897c17
steven <crashoverride025@gmail.com>
2018-06-01 14:36:10 -0500

    More py3 updates.

--- responsys_client/__init__.py
+++ responsys_client/__init__.py
@@ -1,6 +1,2 @@
-from .client import ResponsysClient
-from .exceptions import ResponsysClientError
-from .tests import ResponsysClientTests
-from .utils import convert_to_list_of_dicts
-from .utils import convert_to_table_structure
+
 
